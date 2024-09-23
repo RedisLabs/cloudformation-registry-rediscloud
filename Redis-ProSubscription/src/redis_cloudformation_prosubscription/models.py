b173118528c806467677944a4d5c45399e428de1
florin-nglz-at-191310524501 <florin.manaila@allcloud.io>
2024-09-23 17:57:22 +0300

    Added Pro Subscription extension finished

---
+++ Redis-ProSubscription/src/redis_cloudformation_prosubscription/models.py
@@ -0,0 +1,169 @@
+# DO NOT modify this file by hand, changes will be overwritten
+from dataclasses import dataclass
+
+from cloudformation_cli_python_lib.interface import (
+    BaseModel,
+    BaseResourceHandlerRequest,
+)
+from cloudformation_cli_python_lib.recast import recast_object
+from cloudformation_cli_python_lib.utils import deserialize_list
+
+import sys
+from inspect import getmembers, isclass
+from typing import (
+    AbstractSet,
+    Any,
+    Generic,
+    Mapping,
+    MutableMapping,
+    Optional,
+    Sequence,
+    Type,
+    TypeVar,
+)
+
+T = TypeVar("T")
+
+
+def set_or_none(value: Optional[Sequence[T]]) -> Optional[AbstractSet[T]]:
+    if value:
+        return set(value)
+    return None
+
+
+@dataclass
+class ResourceHandlerRequest(BaseResourceHandlerRequest):
+    # pylint: disable=invalid-name
+    desiredResourceState: Optional["ResourceModel"]
+    previousResourceState: Optional["ResourceModel"]
+    typeConfiguration: Optional["TypeConfigurationModel"]
+
+
+@dataclass
+class ResourceModel(BaseModel):
+    SubscriptionID: Optional[str]
+    BaseUrl: Optional[str]
+    SubscriptionName: Optional[str]
+    DryRun: Optional[str]
+    DeploymentType: Optional[str]
+    PaymentMethod: Optional[str]
+    PaymentMethodId: Optional[str]
+    MemoryStorage: Optional[str]
+    Provider: Optional[str]
+    CloudAccountId: Optional[str]
+    Region: Optional[str]
+    MultipleAvailabilityZones: Optional[str]
+    PreferredAvailabilityZones: Optional[str]
+    DeploymentCIDR: Optional[str]
+    VpcId: Optional[str]
+    DatabaseName: Optional[str]
+    Protocol: Optional[str]
+    DatasetSizeInGb: Optional[str]
+    SupportOSSClusterApi: Optional[str]
+    DataPersistence: Optional[str]
+    Replication: Optional[str]
+    By: Optional[str]
+    Value: Optional[str]
+    LocalThroughputMeasurementRegion: Optional[str]
+    WriteOperationsPerSecond: Optional[str]
+    ReadOperationsPerSecond: Optional[str]
+    ModuleName: Optional[str]
+    ModuleParameters: Optional[str]
+    Quantity: Optional[str]
+    AverageItemSizeInBytes: Optional[str]
+    RespVersion: Optional[str]
+    QueryPerformanceFactor: Optional[str]
+    RedisVersion: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_ResourceModel"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_ResourceModel"]:
+        if not json_data:
+            return None
+        dataclasses = {n: o for n, o in getmembers(sys.modules[__name__]) if isclass(o)}
+        recast_object(cls, json_data, dataclasses)
+        return cls(
+            SubscriptionID=json_data.get("SubscriptionID"),
+            BaseUrl=json_data.get("BaseUrl"),
+            SubscriptionName=json_data.get("SubscriptionName"),
+            DryRun=json_data.get("DryRun"),
+            DeploymentType=json_data.get("DeploymentType"),
+            PaymentMethod=json_data.get("PaymentMethod"),
+            PaymentMethodId=json_data.get("PaymentMethodId"),
+            MemoryStorage=json_data.get("MemoryStorage"),
+            Provider=json_data.get("Provider"),
+            CloudAccountId=json_data.get("CloudAccountId"),
+            Region=json_data.get("Region"),
+            MultipleAvailabilityZones=json_data.get("MultipleAvailabilityZones"),
+            PreferredAvailabilityZones=json_data.get("PreferredAvailabilityZones"),
+            DeploymentCIDR=json_data.get("DeploymentCIDR"),
+            VpcId=json_data.get("VpcId"),
+            DatabaseName=json_data.get("DatabaseName"),
+            Protocol=json_data.get("Protocol"),
+            DatasetSizeInGb=json_data.get("DatasetSizeInGb"),
+            SupportOSSClusterApi=json_data.get("SupportOSSClusterApi"),
+            DataPersistence=json_data.get("DataPersistence"),
+            Replication=json_data.get("Replication"),
+            By=json_data.get("By"),
+            Value=json_data.get("Value"),
+            LocalThroughputMeasurementRegion=json_data.get("LocalThroughputMeasurementRegion"),
+            WriteOperationsPerSecond=json_data.get("WriteOperationsPerSecond"),
+            ReadOperationsPerSecond=json_data.get("ReadOperationsPerSecond"),
+            ModuleName=json_data.get("ModuleName"),
+            ModuleParameters=json_data.get("ModuleParameters"),
+            Quantity=json_data.get("Quantity"),
+            AverageItemSizeInBytes=json_data.get("AverageItemSizeInBytes"),
+            RespVersion=json_data.get("RespVersion"),
+            QueryPerformanceFactor=json_data.get("QueryPerformanceFactor"),
+            RedisVersion=json_data.get("RedisVersion"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_ResourceModel = ResourceModel
+
+
+@dataclass
+class TypeConfigurationModel(BaseModel):
+    RedisAccess: Optional["_RedisAccess"]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_TypeConfigurationModel"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_TypeConfigurationModel"]:
+        if not json_data:
+            return None
+        return cls(
+            RedisAccess=RedisAccess._deserialize(json_data.get("RedisAccess")),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_TypeConfigurationModel = TypeConfigurationModel
+
+
+@dataclass
+class RedisAccess(BaseModel):
+    xapikey: Optional[str]
+    xapisecretkey: Optional[str]
+
+    @classmethod
+    def _deserialize(
+        cls: Type["_RedisAccess"],
+        json_data: Optional[Mapping[str, Any]],
+    ) -> Optional["_RedisAccess"]:
+        if not json_data:
+            return None
+        return cls(
+            xapikey=json_data.get("xapikey"),
+            xapisecretkey=json_data.get("xapisecretkey"),
+        )
+
+
+# work around possible type aliasing issues when variable has same name as a model
+_RedisAccess = RedisAccess
+
+
