f71c20c1f06ff6423076662b8359f9f5e7bd3482
Tim <tim.schaefer24@hotmail.de>
2024-10-11 11:49:11 +0200

    NON BREAKING CHANGE TO NOT TRIGGER WORKFLOW

--- Redis-SubscriptionPeering/src/redis_cloudformation_subscriptionpeering/handlers.py
+++ Redis-SubscriptionPeering/src/redis_cloudformation_subscriptionpeering/handlers.py
@@ -14,7 +14,7 @@ from cloudformation_cli_python_lib import (
 
 from .models import ResourceHandlerRequest, ResourceModel, TypeConfigurationModel
 
-# Use this logger to forward log messages to CloudWatch Logs.
+# Use this logger to forward log messages to CloudWatch Logs. NON BREAKING CHANGE TO NOT TRIGGER WORKFLOW
 LOG = logging.getLogger(__name__)
 TYPE_NAME = "Redis::CloudFormation::SubscriptionPeering"
 
