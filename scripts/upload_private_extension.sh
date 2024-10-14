284920e7d6670d9ae26e3d67109f70d69e4c7af2
Tim Schaefer <tim.schaefer24@hotmail.de>
2024-10-14 13:15:59 +0200

    Testing Pipeline

--- scripts/upload_private_extension.sh
+++ scripts/upload_private_extension.sh
@@ -21,9 +21,12 @@ fi
 Name_Of_Subfolder=$1
 
 echo $1
-echo Name_Of_Subfolder
+echo $Name_Of_Subfolder
 
-# Configure AWS CLI with credentials
+# Configure AWS CLI with environment variables
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_REGION
 
 # List S3 buckets
 echo "Listing S3 buckets..."
