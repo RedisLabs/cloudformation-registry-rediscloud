91f932468cf95fb092f45b5ffa28f5b673945514
Tim <tim.schaefer24@hotmail.de>
2024-10-31 09:40:12 +0100

    Update upload_private_extension.sh

--- scripts/upload_private_extension.sh
+++ scripts/upload_private_extension.sh
@@ -27,12 +27,12 @@ echo $typeName
 # Find the newest extension id and set to local variable
 VERSION_ID=$(aws cloudformation list-type-versions --type RESOURCE --type-name $typeName | jq -r '.TypeVersionSummaries | sort_by(.TimeCreated) | reverse | .[0].VersionId')
 echo 'Newest VERSION_ID'
-echo $VERSION_ID
 
 # If VERSION_ID is unset or empty, set it to '00000001'
 if [ -z "$VERSION_ID" ]; then
     VERSION_ID="00000001"
 fi
+echo $VERSION_ID
 
 # Set the newest version as the default version
 aws cloudformation set-type-default-version --type RESOURCE --type-name $typeName --version-id $VERSION_ID
